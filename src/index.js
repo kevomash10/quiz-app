8c10775e89950762b05f5dc29030d71608522017
kevomash10 <kevomash10@gmail.com>
2020-02-19 12:04:50 +0300

    quizapp first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,89 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, {
+    Component
+} from "react";
+import "./assets/style.css";
+import ReactDOM from "react-dom";
+import quizService from "./quizService";
+import QuestionBox from "./components/QuestionBox"
+import Result from "./components/Result";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class QuizeApp extends Component {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    state = {
+        questionBank: [],
+        score: 0,
+        responses: 0
+    };
+
+    //function to update state
+    getQuestions = () => {
+        quizService().then(question => {
+            this.setState({
+                questionBank: question
+            });
+        });
+    };
+
+    computeAnswer = (answer, correctAnswer) => {
+        if (answer === correctAnswer) {
+            this.setState({
+                score: this.state.score + 1
+            });
+        }
+        this.setState({
+            responses: this.state.responses < 5 ? this.state.responses + 1 : 5
+        });
+    }
+
+    playAgain = () => {
+        this.getQuestions();
+        this.setState({
+            score: 0,
+            responses: 0
+        });
+    }
+    //we need to run this function when our component loads
+    componentDidMount() {
+        this.getQuestions();
+    }
+
+    render() {
+            return ( <
+                div className = "container" >
+                <
+                div className = "title" > QUIZapp < /div> {
+                    this.state.questionBank.length > 0 && this.state.responses < 5 && this.state.questionBank.map(({
+                        question,
+                        answers,
+                        correct,
+                        questionId
+                    }) => ( <
+                        QuestionBox question = {
+                            question
+                        }
+                        options = {
+                            answers
+                        }
+                        key = {
+                            questionId
+                        }
+                        selected = {
+                            answer => this.computeAnswer(answer, correct)
+                        }
+                        />
+                    ))
+                } {
+                    this.state.responses === 5 ? ( < Result score = {
+                            this.state.score
+                        }
+                        playAgain = {
+                            this.playAgain
+                        }
+                        />) : null} <
+                        /div>
+                    );
+
+                }
+
+            }
+            ReactDOM.render( < QuizeApp / > , document.getElementById("root"));
